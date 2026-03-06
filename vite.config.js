8512c2f48dc97be6222b3650a654b23db85478ad
ratanak <ratanak1intel@gmail.com>
2026-03-06 19:33:18 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-
-  build: {
-    outDir: "dist",
-    sourcemap: false,
-    minify: "esbuild",
-
-    chunkSizeWarningLimit: 1000,
-
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          router: ["react-router"],
-          redux: ["react-redux", "@reduxjs/toolkit"],
-        },
-      },
-    },
-  },
-});
+  plugins: [react(), 
+    tailwindcss()],
+})
